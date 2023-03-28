543f5f472fe8a425f06d67063dcc9bdb92d105d3
rustbucket7 <121529306+rustbucket7@users.noreply.github.com>
2023-03-28 16:36:39 -0700

    Added tests for bad inputs

--- enigma_run_tests.cs
+++ enigma_run_tests.cs
@@ -77,7 +77,185 @@ namespace EnigmaTests
             Assert.Equal(EnigmaRun.enigma_run(rotor_choices, plugboard_pairings, initial_rotor_settings, ring_settings, reflector, encrypted_msg), decrypted_msg);
         }
 
+        [Fact]
+        // Bad input string (empty string)
+        public void test_msg4_bad_input()
+        {
+            List<int> rotor_choices = new List<int>(3) { 2, 4, 5 };
+            List<string> plugboard_pairings = new List<string>() { "AV" };
+            List<char> initial_rotor_settings = new List<char>(3) { 'L', 'S', 'D' };
+            List<char> ring_settings = Helpers.list_int_to_list_char(new List<int> { 2, 21, 12 });
+            char reflector = 'B';
+            string encrypted_msg = "";
+            string error_msg = "Bad input string. Letters only.";
 
+            Assert.Equal(EnigmaRun.enigma_run(rotor_choices, plugboard_pairings, initial_rotor_settings, ring_settings, reflector, encrypted_msg), error_msg);
+        }
+
+        [Fact]
+        // Bad input string (only empty spaces)
+        public void test_msg4a_bad_input()
+        {
+            List<int> rotor_choices = new List<int>(3) { 2, 4, 5 };
+            List<string> plugboard_pairings = new List<string>() { "AV" };
+            List<char> initial_rotor_settings = new List<char>(3) { 'L', 'S', 'D' };
+            List<char> ring_settings = Helpers.list_int_to_list_char(new List<int> { 2, 21, 12 });
+            char reflector = 'B';
+            string encrypted_msg = "  ";
+            string error_msg = "Bad input string. Letters only.";
+
+            Assert.Equal(EnigmaRun.enigma_run(rotor_choices, plugboard_pairings, initial_rotor_settings, ring_settings, reflector, encrypted_msg), error_msg);
+        }
+
+        [Fact]
+        // Bad input string (not strictly using letters)
+        public void test_msg4b_bad_input()
+        {
+            List<int> rotor_choices = new List<int>(3) { 2, 4, 5 };
+            List<string> plugboard_pairings = new List<string>() { "AV" };
+            List<char> initial_rotor_settings = new List<char>(3) { 'L', 'S', 'D' };
+            List<char> ring_settings = Helpers.list_int_to_list_char(new List<int> { 2, 21, 12 });
+            char reflector = 'B';
+            string encrypted_msg = "12A@#H";
+            string error_msg = "Bad input string. Letters only.";
+
+            Assert.Equal(EnigmaRun.enigma_run(rotor_choices, plugboard_pairings, initial_rotor_settings, ring_settings, reflector, encrypted_msg), error_msg);
+        }
+
+        [Fact]
+        // Bad input string (not strictly using letters)
+        public void test_msg4c_bad_input()
+        {
+            List<int> rotor_choices = new List<int>(3) { 2, 4, 5 };
+            List<string> plugboard_pairings = new List<string>() { "AV" };
+            List<char> initial_rotor_settings = new List<char>(3) { 'L', 'S', 'D' };
+            List<char> ring_settings = Helpers.list_int_to_list_char(new List<int> { 2, 21, 12 });
+            char reflector = 'B';
+            string encrypted_msg = " A   2   ^^^";
+            string error_msg = "Bad input string. Letters only.";
+
+            Assert.Equal(EnigmaRun.enigma_run(rotor_choices, plugboard_pairings, initial_rotor_settings, ring_settings, reflector, encrypted_msg), error_msg);
+        }
+
+        [Fact]
+        // Bad input string (missing rotor)
+        public void test_msg5_bad_input()
+        {
+            List<int> rotor_choices = new List<int>(3) { 2, 1 };
+            List<string> plugboard_pairings = new List<string>() { "AV" };
+            List<char> initial_rotor_settings = new List<char>(3) { 'L', 'S', 'D' };
+            List<char> ring_settings = Helpers.list_int_to_list_char(new List<int> { 2, 21, 12 });
+            char reflector = 'A';
+            string encrypted_msg = "aaa";
+            string error_msg = "Bad Enigma settings";
+
+            Assert.Equal(EnigmaRun.enigma_run(rotor_choices, plugboard_pairings, initial_rotor_settings, ring_settings, reflector, encrypted_msg), error_msg);
+        }
+
+        [Fact]
+        // Bad input string (too many rotors)
+        public void test_msg5a_bad_input()
+        {
+            List<int> rotor_choices = new List<int>(4) { 2, 1, 3, 4 };
+            List<string> plugboard_pairings = new List<string>() { "AV" };
+            List<char> initial_rotor_settings = new List<char>(3) { 'L', 'S', 'D' };
+            List<char> ring_settings = Helpers.list_int_to_list_char(new List<int> { 2, 21, 12 });
+            char reflector = 'A';
+            string encrypted_msg = "aaa";
+            string error_msg = "Bad Enigma settings";
+
+            Assert.Equal(EnigmaRun.enigma_run(rotor_choices, plugboard_pairings, initial_rotor_settings, ring_settings, reflector, encrypted_msg), error_msg);
+        }
+
+        [Fact]
+        // Bad input string (duplicate plugboard letter Z)
+        public void test_msg6_bad_input()
+        {
+            List<int> rotor_choices = new List<int>(3) { 2, 1, 3};
+            List<string> plugboard_pairings = new List<string>() { "AV", "BZ", "ZF"};
+            List<char> initial_rotor_settings = new List<char>(3) { 'L', 'S', 'D' };
+            List<char> ring_settings = Helpers.list_int_to_list_char(new List<int> { 2, 21, 12 });
+            char reflector = 'A';
+            string encrypted_msg = "aaa";
+            string error_msg = "Bad Enigma settings";
+
+            Assert.Equal(EnigmaRun.enigma_run(rotor_choices, plugboard_pairings, initial_rotor_settings, ring_settings, reflector, encrypted_msg), error_msg);
+        }
+
+        [Fact]
+        // Bad input string (missing initial rotor setting)
+        public void test_msg7_bad_input()
+        {
+            List<int> rotor_choices = new List<int>(3) { 2, 1, 3 };
+            List<string> plugboard_pairings = new List<string>() { "AV", "BZ" };
+            List<char> initial_rotor_settings = new List<char>(3) { 'L', 'S' };
+            List<char> ring_settings = Helpers.list_int_to_list_char(new List<int> { 2, 21, 12 });
+            char reflector = 'A';
+            string encrypted_msg = "aaa";
+            string error_msg = "Bad Enigma settings";
+
+            Assert.Equal(EnigmaRun.enigma_run(rotor_choices, plugboard_pairings, initial_rotor_settings, ring_settings, reflector, encrypted_msg), error_msg);
+        }
+
+        [Fact]
+        // Bad input string (too many initial rotor setting)
+        public void test_msg7a_bad_input()
+        {
+            List<int> rotor_choices = new List<int>(3) { 2, 1, 3 };
+            List<string> plugboard_pairings = new List<string>() { "AV", "BZ" };
+            List<char> initial_rotor_settings = new List<char>(4) { 'L', 'S', 'D', 'A' };
+            List<char> ring_settings = Helpers.list_int_to_list_char(new List<int> { 2, 21, 12 });
+            char reflector = 'A';
+            string encrypted_msg = "aaa";
+            string error_msg = "Bad Enigma settings";
+
+            Assert.Equal(EnigmaRun.enigma_run(rotor_choices, plugboard_pairings, initial_rotor_settings, ring_settings, reflector, encrypted_msg), error_msg);
+        }
+
+        [Fact]
+        // Bad input string (missing ring setting)
+        public void test_msg8_bad_input()
+        {
+            List<int> rotor_choices = new List<int>(3) { 2, 1, 3 };
+            List<string> plugboard_pairings = new List<string>() { "AV", "BZ" };
+            List<char> initial_rotor_settings = new List<char>(3) { 'L', 'S', 'D' };
+            List<char> ring_settings = Helpers.list_int_to_list_char(new List<int> { 2, 21 });
+            char reflector = 'A';
+            string encrypted_msg = "aaa";
+            string error_msg = "Bad Enigma settings";
+
+            Assert.Equal(EnigmaRun.enigma_run(rotor_choices, plugboard_pairings, initial_rotor_settings, ring_settings, reflector, encrypted_msg), error_msg);
+        }
+
+        [Fact]
+        // Bad input string (too many ring setting)
+        public void test_msg8a_bad_input()
+        {
+            List<int> rotor_choices = new List<int>(3) { 2, 1, 3 };
+            List<string> plugboard_pairings = new List<string>() { "AV", "BZ" };
+            List<char> initial_rotor_settings = new List<char>(3) { 'L', 'S', 'D' };
+            List<char> ring_settings = Helpers.list_int_to_list_char(new List<int> { 2, 21, 12, 1 });
+            char reflector = 'A';
+            string encrypted_msg = "aaa";
+            string error_msg = "Bad Enigma settings";
+
+            Assert.Equal(EnigmaRun.enigma_run(rotor_choices, plugboard_pairings, initial_rotor_settings, ring_settings, reflector, encrypted_msg), error_msg);
+        }
+
+        [Fact]
+        // Bad input string (incorrect reflector choice)
+        public void test_msg9_bad_input()
+        {
+            List<int> rotor_choices = new List<int>(3) { 2, 1, 3 };
+            List<string> plugboard_pairings = new List<string>() { "AV", "BZ" };
+            List<char> initial_rotor_settings = new List<char>(3) { 'L', 'S', 'D' };
+            List<char> ring_settings = Helpers.list_int_to_list_char(new List<int> { 2, 21, 12 });
+            char reflector = 'Z';
+            string encrypted_msg = "aaa";
+            string error_msg = "Bad Enigma settings";
+
+            Assert.Equal(EnigmaRun.enigma_run(rotor_choices, plugboard_pairings, initial_rotor_settings, ring_settings, reflector, encrypted_msg), error_msg);
+        }
     }
 
 }
